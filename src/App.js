ac4b0b44d3d91eea117263eb33e80fed620c5b4b
Andresrosalespinzon <andresrosalespinzon@gmail.com>
2021-01-10 09:29:46 +0100

    Added handler to the input so I can track it with state (useState)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 //Components
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
   return (
     <div className="App">
       <header>
         <h1>Andre's Todo List</h1>
       </header>
       <Form />
+      <TodoList />
     </div>
   );
 }
