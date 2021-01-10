9769e4e293ff604fa9dca1a20589c3114c29f3d7
Andresrosalespinzon <andresrosalespinzon@gmail.com>
2021-01-10 09:15:03 +0100

    added components, imported/exported to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+//Components
+import Form from './components/Form';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Andre's Todo List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
